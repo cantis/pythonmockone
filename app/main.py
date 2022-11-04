bff805c6a99c079b41c08489c8bdbbcb49f51242
Evan Young <cantis@gmail.com>
2022-11-04 01:34:10 -0500

    Intermediate Commit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import psycopg2
+
+from models import Book, Base
+
+load_dotenv()
+
+username = os.getenv("POSTGRES_USERNAME")
+password = os.getenv("POSTGRES_PASSWORD")
+database = os.getenv("POSTGRES_DATABASE")
+
+posgres_url = f"postgres+psycopg2://{username}:{password}@localhost:5432/{database}"
+
+engine = create_engine(posgres_url)
+
+Base.metadata.create_all(engine)
+
 def dependant_value() -> int:
     return 48
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+#Python Mock One
+Learning the Python PyTest Mock library
+
+**2022-10-21**
+I've been able to get a basic mock working, not sure if I'm doing it 'right' but the patch works.
+
+**2022-11-04**
+Continuing on, now working on mocking a postgres database, first thing I'll do is add a test database and get the read working 'normally' then I'll mock a different value and the test should return the different value. 
+
